3dd99efa898a4e0037a88a6ac98d19490286ee97
nestorss05 <nestor.sanchez@iesnervion.es>
2024-02-22 13:41:01 +0100

    First commit

---
+++ src/base/MainCode.java
@@ -0,0 +1,19 @@
+package base;
+
+/**
+ * MainCode class
+ * @author Group5
+ */
+public class MainCode {
+
+	/**
+	 * Main function
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
