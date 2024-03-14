faa75d255da29ec0c0aac7f89ff2307ac42b7919
nestorss05 <nestor.sanchez@iesnervion.es>
2024-03-14 13:26:26 +0100

    Updated Fly class to equal it to branch alonso's Fly class

---
+++ src/base/Fly.java
@@ -0,0 +1,26 @@
+package base;
+/**
+ * Class that represents a Fly. 
+ * It contains healthpoints and name attributes.
+ * @author Alonso
+ * @author Nestor
+ * */
+public class Fly {
+	/**
+	 * Health points of the fly
+	 */
+	private int healthPoints;
+	
+	/**
+	 * Name of the fly
+	 */
+	private String name = "";
+	
+	/**
+	 * HP setter
+	 * @param numberHP number of health points to assign to the fly
+	 */
+	public void setHealthPoints (int hp) {
+		
+	}
+}
