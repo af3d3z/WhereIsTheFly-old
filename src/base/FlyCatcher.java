78682524e31cf84d2ade45e26d69f8a3f7dd8b79
nestorss05 <nestor.sanchez@iesnervion.es>
2024-03-14 14:18:30 +0100

    Unfinished FlyCatcher class

---
+++ src/base/FlyCatcher.java
@@ -0,0 +1,15 @@
+package base;
+
+/**
+ * FlyCatcher class
+ * @author alonso
+ * @author nestor
+ */
+public class FlyCatcher {
+
+	public static boolean hitFly(int x, int y) {
+		boolean catched = false;
+		
+	}
+	
+}
