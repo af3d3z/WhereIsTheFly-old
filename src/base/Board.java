d8063e290fc537b8b6fa208696e10ecef78de15b
nestorss05 <nestor.sanchez@iesnervion.es>
2024-04-25 13:46:30 +0200

    Created the fly-catcher / deleter

--- src/base/Board.java
+++ src/base/Board.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import base.board.items.BoardItems;
 
 /**
- * Class that simulates the board where the flies are gonna be, the unofficial "base" of the game
+ * Class that simulates the board where the flies are gonna be, the unofficial
+ * "base" of the game
  * 
  * @author DragonaEileen
  */
@@ -13,25 +14,27 @@ public class Board {
 
 	/* Fields */
 	/**
-	 * Bidimensional array where all the flies and power ups and downs are going to be placed
+	 * Bidimensional array where all the flies and power ups and downs are going to
+	 * be placed
 	 */
 	protected BoardItems[][] gameBoard;
-	
+
 	/**
-	 * 1-dimensional array list with dinamic length to access the flies on the board quickly
+	 * 1-dimensional array list with dinamic length to access the flies on the board
+	 * quickly
 	 */
 	protected ArrayList<Fly> flyArray = new ArrayList<Fly>();
-	
+
 	/**
 	 * Hit radius of the flycatcher
 	 */
 	protected int areaRadius = 0;
-	
+
 	/**
 	 * Hit damage of the flycatcher
 	 */
 	protected int hitDamage = 1;
-	
+
 	/* Constructors */
 	/**
 	 * Constructor with parameters
@@ -39,30 +42,30 @@ public class Board {
 	 * @param difficulty Difficulty of the game that affects the size of the board
 	 */
 	public Board(int difficulty) {
-		
-			/* Base of Axis I */
-		int randomPowerAxisI = (int) (Math.random()*2 + 2);
-			
-			/* Size of Axis I */
+
+		/* Base of Axis I */
+		int randomPowerAxisI = (int) (Math.random() * 2 + 2);
+
+		/* Size of Axis I */
 		int randomAxisI = (int) Math.pow(randomPowerAxisI, difficulty);
-		
-			/* Base of Axis J */
-		int randomPowerAxisJ = (int) (Math.random()*2 + 2);
-			
-			/* Size of Axis I */
+
+		/* Base of Axis J */
+		int randomPowerAxisJ = (int) (Math.random() * 2 + 2);
+
+		/* Size of Axis I */
 		int randomAxisJ = (int) Math.pow(randomPowerAxisJ, difficulty);
-		
-		//Now we set the size
+
+		// Now we set the size
 		this.gameBoard = new BoardItems[randomAxisI][randomAxisJ];
-		
-		//Lets generate the flies
+
+		// Lets generate the flies
 		this.generateFlies();
-		
-		//Now lets fill the board with'em
+
+		// Now lets fill the board with'em
 		this.fillBoard();
-		
-	}//Fin Constructor With Parameters
-	
+
+	}// Fin Constructor With Parameters
+
 	/* Setter */
 	/**
 	 * We ought to fill the cells with something
@@ -72,11 +75,11 @@ public class Board {
 	 * @param posJ The position of the cell on the J axis
 	 */
 	public void setBoardCell(BoardItems item, int posI, int posJ) {
-		
+
 		gameBoard[posI][posJ] = item;
-		
-	}//Fin setBoardCell()
-	
+
+	}// Fin setBoardCell()
+
 	/* Methods */
 	/**
 	 * Method that returns true if there's a fly int the given position
@@ -86,136 +89,182 @@ public class Board {
 	 * @return thereIsFly True if there's indeed a fly or false if not
 	 */
 	public boolean isThereFlyHere(int posI, int posJ) {
-		
+
 		/* PCC */
 		/* Boolean to return */
 		boolean thereIsFly = false;
-		
+
 		/* Checking */
-		/* Lets go through the Fly Array seeking if the fly on the array is equal to the one at the position */
-		for(Fly flutter: this.flyArray) {
-			
-			//If to seek similarity
-			if(flutter.equals(gameBoard[posI][posJ])) {
-				
+		/*
+		 * Lets go through the Fly Array seeking if the fly on the array is equal to the
+		 * one at the position
+		 */
+		for (Fly flutter : this.flyArray) {
+
+			// If to seek similarity
+			if (flutter.equals(gameBoard[posI][posJ])) {
+
 				thereIsFly = true;
-				
-			}//Fin IF --> Checking if equals
-			
-		}//Fin FOR --> Flies running through
-		
+
+			} // Fin IF --> Checking if equals
+
+		} // Fin FOR --> Flies running through
+
 		/* Return Statement */
 		return thereIsFly;
-		
-	}//Fin isThereFlyHere?
-	
+
+	}// Fin isThereFlyHere?
+
 	/**
 	 * Filling method, it generates the flies and inserts them into the board
 	 * 
 	 * @param difficulty Difficulty of the Game
 	 */
 	protected void fillBoard() {
-		
+
 		/* Random position on the I axis */
 		int randPosI;
-		
+
 		/* Random position on the J axis */
 		int randPosJ;
-		
+
 		/* Size of axis I */
 		int sizeI = this.gameBoard.length;
-		
+
 		/* Size of axis J */
 		int sizeJ = this.gameBoard[0].length;
-		
+
 		/* Flag to continue the loop */
 		boolean flag;
-		
+
 		/* Let's go through the flyArray inserting flies */
-		for(Fly flutter: flyArray) {
-			
-			//Reset of the flag
+		for (Fly flutter : flyArray) {
+
+			// Reset of the flag
 			flag = true;
-			
-			while(flag) {
-				
-				//Random asignation of position
-				randPosI = (int) (Math.random()*sizeI);
-				randPosJ = (int) (Math.random()*sizeJ);
-				
-				//Lets see if there is a fly already there
-				if(!this.isThereFlyHere(randPosI, randPosJ)) {
-					
-					//Let's insert it!!!
+
+			while (flag) {
+
+				// Random asignation of position
+				randPosI = (int) (Math.random() * sizeI);
+				randPosJ = (int) (Math.random() * sizeJ);
+
+				// Lets see if there is a fly already there
+				if (!this.isThereFlyHere(randPosI, randPosJ)) {
+
+					// Let's insert it!!!
 					this.gameBoard[randPosI][randPosJ] = flutter;
-					
-					//Flag goes weeee
+
+					// Flag goes weeee
 					flag = false;
-					
-				}//Fin IF --> Checking for flies already there
-				
-			}//Fin WHILE --> Inserting loop
-				
-		}//Fin FOR --> Going through the flies
-		
-	}//Fin fillBoard()
+
+				} // Fin IF --> Checking for flies already there
+
+			} // Fin WHILE --> Inserting loop
+
+		} // Fin FOR --> Going through the flies
+
+	}// Fin fillBoard()
 
 	/**
 	 * Method to generate the flies
 	 */
 	protected void generateFlies() {
-		
+
 		/* PCC */
-		/* Number of flies that are gonna be inserted
-		 * It's gonna be equal to 25% of the total of cells */
+		/*
+		 * Number of flies that are gonna be inserted It's gonna be equal to 25% of the
+		 * total of cells
+		 */
 		int totalFlies = (int) (this.gameBoard.length * this.gameBoard[0].length * 0.25);
-		
+
 		/* Fly to add declaration */
 		Fly flutter;
-		
+
 		/* Lets fill first the fly array */
-		for(int i = 0; i < totalFlies; i++) {
-			
-			//Let's construct the fly
+		for (int i = 0; i < totalFlies; i++) {
+
+			// Let's construct the fly
 			flutter = new Fly();
-			
-			//And add it to the array
-			this.flyArray.add(flutter); //here you add the fly
-			
-		}//Fin FOR --> Stuffing fly array
-		
-	}//Fin generateFlies()
-	
+
+			// And add it to the array
+			this.flyArray.add(flutter); // here you add the fly
+
+		} // Fin FOR --> Stuffing fly array
+
+	}// Fin generateFlies()
+
+	/**
+	 * Method to catch a fly given the position
+	 * 
+	 * @param posI: position I from the array
+	 * @param posJ: position J from the array
+	 * @return if a fly has been catched or not
+	 */
+	public boolean catchFly(int posI, int posJ) {
+
+		// Boolean catched: indicates if the fly has been hit or not
+		boolean catched = false;
+
+		// Check if the fly has been catched or not
+		catched = this.isThereFlyHere(posI, posJ);
+
+		// If: if the fly has been catched:
+		if (catched) {
+
+			// For Each: loop through flyArray and check which fly has been hit
+			for (Fly flutter : flyArray) {
+
+				// If the fly's position coincides with the one that has been hit...
+				if (flutter.equals(gameBoard[posI][posJ])) {
+
+					// Reduce the points via hitFly
+					flutter.hitFly(hitDamage);
+
+					// If: if the health points are 0, the fly will be removed.
+					if (flutter.getHealthPoints() < 1) {
+						gameBoard[posI][posJ] = null;
+						flyArray.remove(flutter);
+					} // End If
+
+				} // End If
+
+			} // End For Each
+
+		} // End If
+
+		// Return the variable catched
+		return catched;
+
+	}
+
+	// PRINT METHODS
+
 	/**
 	 * Method to print the board
 	 */
 	public void printingBoard() {
-		
-		/* Counter: cell number */
-		int count = 1;
-		
+
 		/* String to print */
 		String toPrint = "";
-		
+
 		/* Lets go through the I axis */
-		for(int i = 0; i < this.gameBoard.length; i++) {
-			
+		for (int i = 0; i < this.gameBoard.length; i++) {
+
 			/* Now, through the J axis */
-			for(int j = 0; j < this.gameBoard[i].length; j++) {
-				
-				toPrint += "[" + count + "]";
-				
-				count += 1;
-				
-			}//Fin FOR --> Axis J
-			
+			for (int j = 0; j < this.gameBoard[i].length; j++) {
+
+				toPrint += "[" + i + "," + j + "]";
+
+			} // Fin FOR --> Axis J
+
 			toPrint += "\n";
-			
-		}//Fin FOR --> Axis I
-		
-		//Printing
+
+		} // Fin FOR --> Axis I
+
+		// Printing
 		System.out.println(toPrint);
-		
-	}//Fin printingBoard()
-	
+
+	}// Fin printingBoard()
+
 }
--- src/base/Fly.java
+++ src/base/Fly.java
@@ -3,87 +3,101 @@ package base;
 import base.board.items.BoardItems;
 
 /**
- * Class that represents a Fly. 
- * It contains healthpoints and name attributes.
+ * Class that represents a Fly. It contains healthpoints and name attributes.
+ * 
  * @author Alonso
  * @author Nestor
- * */
-public class Fly extends BoardItems{
+ */
+public class Fly extends BoardItems {
 	/**
 	 * List of possible names for the flies
 	 */
-	private String[] nameList = {
-		"Gaga", "Lindsay", "Eusebio", "Kafka", "Jeff Goldbloom", "Jennifer", "Ana L.M.", "Pepa the fool", "Flygon"	
-	};
-	
+	private String[] nameList = { "Gaga", "Lindsay", "Eusebio", "Kafka", "Jeff Goldbloom", "Jennifer", "Ana L.M.",
+			"Pepa the fool", "Flygon" };
+
 	/**
 	 * Health points of the fly
 	 */
 	private int healthPoints;
-	
+
 	/**
 	 * Name of the fly
 	 */
 	private String name = "";
-	
+
 	/**
 	 * modifies the healthpoints of the fly
+	 * 
 	 * @param hp number of health points to assign to the fly
 	 */
-	public void setHealthPoints (int hp) {
-		if(hp > 0) {
-			this.healthPoints = hp;			
+	public void setHealthPoints(int hp) {
+		if (hp > 0) {
+			this.healthPoints = hp;
 		}
 	}
-	
+
 	/**
 	 * returns the fly health points
+	 * 
 	 * @return Fly's health points
 	 */
-	public int getHealthPoints () {
+	public int getHealthPoints() {
 		return this.healthPoints;
 	}
-	
+
 	/**
 	 * Modifies the name of the fly
+	 * 
 	 * @param name Name for the fly
 	 */
 	public void setName(String name) {
-		if(!name.trim().equals("")) {
+		if (!name.trim().equals("")) {
 			this.name = name;
 		}
 	}
-	
+
 	/**
 	 * Returns the name of the fly
+	 * 
 	 * @return name of the fly
-	 * */
+	 */
 	public String getName() {
 		return this.name;
 	}
-	
+
 	/**
 	 * Gets a random name from nameList
+	 * 
 	 * @return Random name
-	 * */
+	 */
 	public String getRandomName() {
-		return nameList[(int) Math.random() * nameList.length];	     
+		return nameList[(int) Math.random() * nameList.length];
 	}
-	
+
 	/**
 	 * Give random health points to the fly
-	 * @return 
-	 * */
+	 * 
+	 * @return
+	 */
 	public int getRandomHealthPoints() {
 		int hp = 1 + ((int) Math.random() * 2);
 		return hp;
 	}
-	
+
 	/**
 	 * Constructor con nombres y puntos de vida aleatorios
-	 * */
-	public Fly () {
+	 */
+	public Fly() {
 		this.name = getRandomName();
 		this.healthPoints = getRandomHealthPoints();
+	}
+
+	/**
+	 * Method hitFly: decreases the health points of a fly
+	 * 
+	 * @param hp
+	 */
+	public void hitFly(int hp) {
+		this.healthPoints = this.healthPoints - hp;
 	}
 }
